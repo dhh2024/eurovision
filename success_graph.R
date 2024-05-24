3ea939c95acc9c83d096f19986f230bf4e8c8102
Jonas Berg <jonas.berg@relex.fi>
2024-05-24 11:55:31 +0300

    updated graph code, added spotify data for reproducibility

--- success_graph.R
+++ success_graph.R
@@ -3,7 +3,7 @@ slices <- c(1/3, 1/6, 1/6, 1/3)
 lbls <- c("Jury votes 33%", "Spotify popularity 17%", "YouTube views 17%", "Televote 33%")
 
 png(filename="success_pie_with_labels.png", bg = "transparent")
-pie(slices,labels = lbls, col=hcl.colors(length(lbls), palette = "Heat 2"),
+pie(slices, init.angle=90, labels = lbls, col=hcl.colors(length(lbls), palette = "Heat 2"),
     main="Weights of success factors")
 dev.off()
 png(filename="success_pie_without_labels.png", bg = "transparent")
--- youtube_boxplot.R
+++ youtube_boxplot.R
@@ -1,13 +1,26 @@
 library(ggplot2)
+library(scales)
 table <- read.csv(file="sorted-enriched-contestants2004-2024.csv", header=TRUE, sep=",")
 toplist <- table[1:40, ]
 toplist$class <- "Top 40"
 randomlist <- read.csv(file="random40.csv", header=TRUE, sep=",")
 randomlist$class <-"Random 40"
 combined <- rbind(toplist, randomlist)
-head(mtcars)
+
+# Given a vector x, return a vector of powers of 10 that encompasses all values
+# in x.
+breaks_log10 <- function(x) {
+  low <- floor(log10(min(x)))
+  high <- ceiling(log10(max(x)))
+  
+  10^(seq.int(low, high))
+}
+
+png(filename="youtube_boxplot.png", bg = "transparent")
 ggplot(combined, aes(x=as.factor(class), y=views, fill=as.factor(class))) + 
-  geom_boxplot(fill=c("orange", "magenta")) + labs(title = "Youtube views by '40' Groups", x = "", y = "Youtube views") +
- xlab("") + ylab("Total views") + scale_fill_manual(values = c("orange", "magenta")) +
+  geom_boxplot(fill=c("orange", "magenta")) + labs(title = "Youtube Views by '40' Groups", x = "", y = "log10(Youtube views)") +
+ xlab("") + ylab("Total Views") + scale_fill_manual(values = c("orange", "magenta")) +
   theme_minimal()+
-  theme(legend.position = "none")
+  theme(text = element_text(size=20),legend.position = "none",axis.ticks.x = element_blank()) + annotation_logticks(sides = "lr") +  scale_y_log10(breaks = breaks_log10,
+                                                                                                        labels = trans_format(log10, math_format(10^.x)))
+dev.off()
